9b1a8d0cf3cae7b628fab77baa2bbdb380fc5403
Tamas Nepusz <ntamas@gmail.com>
2020-07-02 16:58:12 +0200

    added separate configuration for deployment on share.skybrush.io

---
+++ webpack/dist.config.js
@@ -0,0 +1,35 @@
+// Webpack configuration for the output that is directly usable on
+// https://share.skybrush.io
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const merge = require('webpack-merge');
+
+const baseConfig = require('./base.config.js');
+const { htmlMetaTags, projectRoot } = require('./helpers');
+
+module.exports = merge.smart(baseConfig, {
+  // Make sure to use a _single_ entry point here; we want a single bundle.js
+  // in the browser-based deployment for sake of simplicity
+  entry: ['@babel/polyfill', 'whatwg-fetch', './src/index'],
+  output: {
+    filename: 'bundle.js',
+    publicPath: '/build/',
+  },
+
+  resolve: {
+    alias: {
+      config: path.resolve(projectRoot, 'config', 'webapp'),
+    },
+  },
+
+  plugins: [
+    // Create index.html on-the-fly
+    new HtmlWebpackPlugin({
+      meta: htmlMetaTags,
+      template: path.resolve(projectRoot, 'index.html'),
+      title:
+        'Skybrush Viewer | The Next-generation Drone Light Show Software Suite',
+    }),
+  ],
+});

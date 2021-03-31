afb3e1d76b75bc8332eb8cdae0af936b29283185
Tamas Nepusz <ntamas@gmail.com>
2021-03-31 16:33:39 +0200

    chore: fix indentation in webpack/launcher.config.js

--- webpack/launcher.config.js
+++ webpack/launcher.config.js
@@ -3,12 +3,12 @@ const { merge } = require('webpack-merge');
 const baseConfig = require('./base.config.js');
 
 const plugins = [
-    new webpack.DefinePlugin({
-        // work around a misbehaving debug() module that tries to assign to
-        // process.env.DEBUG. Also needs a global variable named
-        // __runtime_process_env
-        'process.env.DEBUG': '__runtime_process_env.DEBUG'
-    })
+  new webpack.DefinePlugin({
+    // work around a misbehaving debug() module that tries to assign to
+    // process.env.DEBUG. Also needs a global variable named
+    // __runtime_process_env
+    'process.env.DEBUG': '__runtime_process_env.DEBUG',
+  }),
 ];
 
 module.exports = merge(baseConfig, {

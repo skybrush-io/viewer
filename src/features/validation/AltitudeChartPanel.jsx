efd41cd7654b5bb3e20557926b42dee4f3a27b31
Tamas Nepusz <ntamas@gmail.com>
2021-08-05 13:39:01 +0200

    fix: altitude chart should have its lowest maximum at 2m when the show is indoor

--- src/features/validation/AltitudeChartPanel.jsx
+++ src/features/validation/AltitudeChartPanel.jsx
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 
-import { getTimestampFormatter } from '~/features/show/selectors';
+import { getTimestampFormatter, isShowIndoor } from '~/features/show/selectors';
 
 import ChartPanel from './ChartPanel';
 import {
@@ -13,17 +13,18 @@ const getDataForAltitudeChart = createChartDataSelector(
   getSampledAltitudesForDrones
 );
 
-// Custom range to use for the chart panel. This prevents the annotations from
+// Custom ranges to use for the chart panel. This prevents the annotations from
 // affecting the range chosen by Chart.js but it will still allow the data to
 // expand the range if needed.
-const Y_RANGE = [0, 10];
+const Y_RANGE_INDOOR = [0, 2];
+const Y_RANGE_OUTDOOR = [0, 10];
 
 export default connect(
   // mapStateToProps
   (state) => ({
     data: getDataForAltitudeChart(state),
     formatPlaybackTimestamp: getTimestampFormatter(state),
-    range: Y_RANGE,
+    range: isShowIndoor(state) ? Y_RANGE_INDOOR : Y_RANGE_OUTDOOR,
     threshold: getAltitudeWarningThreshold(state),
     thresholdLabel: 'Altitude threshold',
     title: 'Altitudes',

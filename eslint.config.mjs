bfd0311d95b845a7c6e535989ed7ae6ec213240c
Tamas Nepusz <ntamas@gmail.com>
2025-12-11 20:38:08 +0100

    refactor: out with xo, in with typescript-eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  globalIgnores(['build'])
+);

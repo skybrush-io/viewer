5859aeca191d7764ec908827efa4d14051946e16
Tamas Nepusz <ntamas@gmail.com>
2025-12-22 16:11:38 +0100

    refactor: preloader is now ESM

--- src/desktop/preload/index.mjs
+++ src/desktop/preload/index.mjs
@@ -1,15 +1,14 @@
-const { contextBridge } = require('electron');
-const { ipcRenderer: ipc } = require('electron-better-ipc');
+import { ipcRenderer as ipc } from 'electron-better-ipc';
+import { contextBridge } from 'electtron';
+import createStorageEngine from 'redux-persist-electron-storage';
 
-const createStorageEngine = require('redux-persist-electron-storage');
-
-const { receiveActionsFromRenderer, setupIpc } = require('./ipc');
+import { receiveActionsFromRenderer, setupIpc } from './ipc';
 
 /**
  * Creates a Redux state store object that stores the Redux state in an
  * Electron store.
  *
- * @return {Object}  a Redux storage engine that can be used by redux-storage
+ * @return  a Redux storage engine that can be used by redux-storage
  */
 function createStateStore() {
   return createStorageEngine({
---
+++ src/desktop/preload/ipc.mjs
@@ -0,0 +1,52 @@
+import { ipcRenderer as ipc } from 'electron-better-ipc';
+
+/**
+ * @type {Record<string, (...args: any[]) => unknown>}
+ */
+const actionsFromRenderer = {};
+
+const noop = () => {
+  /* intentionally left empty */
+};
+
+/**
+ * @param {string} name
+ * @returns {(...args: any[]) => unknown}
+ */
+const getActionByName = (name) => {
+  const func = actionsFromRenderer[name];
+  if (func) {
+    return func;
+  }
+
+  console.warn(`${name}() action was not provided by the renderer`);
+  return noop;
+};
+
+/**
+ * @param {string} name
+ * @returns {(...args: any[]) => unknown}
+ */
+const createActionProxy =
+  (name) =>
+  (...args) => {
+    // @ts-expect-error - dynamic access
+    const action = getActionByName(name);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    return action(...args);
+  };
+
+export const receiveActionsFromRenderer = (actions) => {
+  Object.assign(actionsFromRenderer, actions);
+};
+
+export const setupIpc = () => {
+  ipc.answerMain(
+    'notifyFileOpeningRequest',
+    createActionProxy('loadShowFromLocalFile')
+  );
+
+  ipc.answerMain('loadShowFromObject', createActionProxy('loadShowFromObject'));
+
+  ipc.answerMain('setUIMode', createActionProxy('setUIMode'));
+};

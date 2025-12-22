94ed40d0ab871ce5c41ce8273f260c95f6384c4c
Tamas Nepusz <ntamas@gmail.com>
2025-12-22 18:00:59 +0100

    refactor: use patch-package to patch node-ssdp instead of monkeypatching

--- DEPENDENCIES.md
+++ DEPENDENCIES.md
@@ -4,13 +4,4 @@ This document lists the reasons why specific dependencies are pinned down to
 exact versions. Make sure to consider these points before updating dependencies
 to their latest versions.
 
-## `node-ssdp`
-
-We monkey-patch one of the methods of the `Server` class from `node-ssdp`. To
-ensure that the patch works, we pinned `node-ssdp` down to the exact version
-where we know the patch works. Note that there were no new releases from
-`node-ssdp` since 2020 at the time of writing (Dec 2025) so it is not likely
-to be a problem.
-
-Should we need to update `node-ssdp` in the future, we can consider using
-`patch-package` instead of monkey-patching.
+No dependencies to list yet.
--- src/desktop/launcher/http-server.mjs
+++ src/desktop/launcher/http-server.mjs
@@ -1,6 +1,4 @@
-import { createSocket } from 'node:dgram';
 import http from 'node:http';
-import { networkInterfaces } from 'node:os';
 import process from 'node:process';
 
 import express from 'express';
@@ -8,89 +6,9 @@ import { Server as SSDPServer } from 'node-ssdp';
 
 import apiV1 from './api-v1.mjs';
 
-function isOwnIPAddress(address) {
-  const nets = networkInterfaces();
-
-  for (const name of Object.keys(nets)) {
-    for (const net of nets[name]) {
-      if (net.address === address) {
-        return true;
-      }
-    }
-  }
-
-  return false;
-}
-
-// Monkey-patch SSDPServer._send so we never send advertisements and we respond
-// to requests from our own IP only
-
 /**
- * @typedef {(message: unknown, host: string, port: number, cb: (error?: any) => void) => void} SSDPServerSendFn
+ * @param {number} port  the port on which our own HTTP server will be listening
  */
-
-/** @type {SSDPServerSendFn} */
-const _originalSend = SSDPServer.prototype._send;
-
-/**
- * @param {unknown} message
- * @param {string} host
- * @param {number} port
- * @param {(error?: any) => void} cb
- */
-SSDPServer.prototype._send = function (message, host, port, cb) {
-  if (typeof host === 'function') {
-    // This is an advertisement
-    cb = host;
-    cb();
-  } else if (host === '127.0.0.1' || isOwnIPAddress(host)) {
-    // Check whether the host IP address is ours; we don't respond to requests
-    // coming from another machine
-    _originalSend.call(this, message, host, port, cb);
-  } else {
-    cb();
-  }
-};
-
-// Monkeypatch SSDPServer so we create sockets for local and non-local interfaces
-// alike. This is because we need a socket on localhost even if no other
-// interface is available. However, we also need non-local interfaces because
-// older versions of the Skybrush Studio for Blender add-on look for Skybrush
-// Viewer in the "real" IP addresses of the network interfaces only, not on
-// 127.0.0.1. This was fixed in versions 1.9.2 and 1.10.0 of the Blender add-on.
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-SSDPServer.prototype._createSockets = function () {
-  const interfaces = networkInterfaces();
-
-  for (const iName of Object.keys(interfaces)) {
-    if (this._interfaces && this._interfaces.includes(iName)) {
-      continue;
-    }
-
-    this._logger('discovering all IPs from interface %s', iName);
-
-    for (const ipInfo of interfaces[iName]) {
-      if (ipInfo.family === 'IPv4') {
-        const socket = createSocket({
-          type: 'udp4',
-          reuseAddr: this._reuseAddr,
-        });
-        if (socket) {
-          socket.unref();
-          this.sockets[ipInfo.address] = socket;
-        }
-      }
-    }
-  }
-
-  if (Object.keys(this.sockets).length === 0) {
-    throw new Error('No sockets available, cannot start.');
-  }
-};
-/* eslint-enable @typescript-eslint/no-unsafe-argument */
-/* eslint-enable @typescript-eslint/no-unsafe-call */
-
 const setupSSDPDiscovery = async (port) => {
   const UPNP_DEVICE_ID = `urn:collmot-com:device:skybrush-viewer:1`;
   const UPNP_SERVICE_ID = `urn:collmot-com:service:skyc-validator:1`;
@@ -104,28 +22,11 @@ const setupSSDPDiscovery = async (port) => {
   try {
     await server.start();
     process.on('exit', () => server.stop());
-  } catch {
+  } catch (err) {
     console.warn('No SSDP sockets were created; SSDP discovery is disabled.');
+    console.error(err);
   }
 };
-
-/**
- * @type {Express.ErrorRequestHandler}
- */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-const logErrors = (error, _req, res, next) => {
-  console.log(error);
-
-  if (res.headersSent) {
-    return next(error);
-  }
-
-  res.status(500);
-  res.json({ error: String(error) || 'Unexpected error' });
-};
-/* eslint-enable @typescript-eslint/no-unsafe-return */
-/* eslint-enable @typescript-eslint/no-unsafe-call */
 
 export const setupHttpServer = async ({ port = 0 } = {}) => {
   const app = express();
@@ -137,12 +38,27 @@ export const setupHttpServer = async ({ port = 0 } = {}) => {
   app.set('port', port);
 
   app.use(
-    express.raw({ type: 'application/skybrush-compiled', limit: '16mb' })
+    express.raw({ type: 'application/skybrush-compiled', limit: '128mb' })
   );
 
   app.use('/api/v1', apiV1);
 
-  app.use(logErrors);
+  /* eslint-disable @typescript-eslint/no-unsafe-call */
+  /* eslint-disable @typescript-eslint/no-unsafe-return */
+  app.use((error, _req, res, next) => {
+    console.log(error);
+
+    if (res.headersSent) {
+      return next(error);
+    }
+
+    res.status(500);
+    res.json({ error: String(error) || 'Unexpected error' });
+  });
+  /* eslint-enable @typescript-eslint/no-unsafe-return */
+  /* eslint-enable @typescript-eslint/no-unsafe-call */
+
+  console.log(`Skybrush Viewer HTTP server listening on port ${port}`);
 };
 
 export default setupHttpServer;
